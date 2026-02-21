e1248f87d7181abad23fee5e78fb5b1f2f713d5f
jrjagdish <hanamshettyjagdish1@gmail.com>
2026-02-21 14:47:07 +0530

    local set up is done

--- db/base.py
+++ db/base.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
 from db.models import Base
 from sqlalchemy.orm import sessionmaker
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
+database_url = os.getenv("DATABASE_URL")
 engine = create_engine(
-    "sqlite:///app.db", echo=True, connect_args={"check_same_thread": False}
+    database_url, echo=True, connect_args={"check_same_thread": False}
 )
 Base.metadata.create_all(bind=engine)
 
